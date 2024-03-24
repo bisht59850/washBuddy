dac91c77b72540aeaaa843219831ed8bb74c9b72
Vijay singh <vijaymessibisht@gmail.com>
2024-03-24 20:20:18 +0530

    flutter Tutorial

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('Flutter Container'),
+      ),
+      body:Center(
+        child: Container(
+          width: 150,
+          height: 150,
+          color: Colors.blueGrey,
+          child: Center(child: Text('Hello Vijay',style: TextStyle(fontSize: 25,color: Colors.white),)),
+
+        ),
+
+      ),
+       // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
